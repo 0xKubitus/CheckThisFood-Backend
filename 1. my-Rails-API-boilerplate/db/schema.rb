a24e4ceacb154d343c1469068a5b28f4a55ed304
0xKubitus <n.cubilier@gmail.com>
2022-09-20 16:38:57 +0200

    ajout des modifs de la PR d'Eva non-validée avant le split du repo CheckThisFood en 2 repo séparés

--- 1. my-Rails-API-boilerplate/db/schema.rb
+++ 1. my-Rails-API-boilerplate/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_085627) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_144601) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_085627) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+  end
+
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti"
     t.datetime "exp"
@@ -66,7 +75,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_085627) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -75,4 +83,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_085627) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "recipes"
 end
